d040304722b4da6d03b298b73415d929272d5127
yanger <kanayaofficial1129@gmail.com>
2024-06-06 18:18:59 +0900

    Modify routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "static_pages#landing"
+
+  get 'login', to: 'user_sessions#new'
+  post 'login', to: 'user_sessions#create'
+  delete 'logout', to: 'user_sessions#destroy'
+
+  resources :users, only: [:new, :create]
 end
