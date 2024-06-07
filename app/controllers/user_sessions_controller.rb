20a7e667ea82dfa1df41bfa9d40b394bde9cfae2
yanger <kanayaofficial1129@gmail.com>
2024-06-07 22:53:16 +0900

    Add flash_message in user_sessions_controller

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -4,14 +4,15 @@ class UserSessionsController < ApplicationController
   def create
     @user = login(params[:email], params[:password])
     if @user
-      redirect_back_or_to root_path
+      redirect_back_or_to root_path, success: t('.success')
     else
+      flash.now[:danger] = t('.danger')
       render :new
     end
   end
 
   def destroy
     logout
-    redirect_to root_path, status: :see_other
+    redirect_to root_path, status: :see_other, success: t('.success')
   end
 end
