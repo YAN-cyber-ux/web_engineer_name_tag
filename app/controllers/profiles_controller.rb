ef93c02143810ca9dbbeba11e932ba264b25dc1a
yanger <kanayaofficial1129@gmail.com>
2024-06-11 00:19:45 +0900

    Add profiles controller

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,2 +1,20 @@
 class ProfilesController < ApplicationController
+  before_action :set_profile, only: %i[ edit update ]
+
+  def edit; end
+
+  def show; end
+
+  def update
+  end
+
+  private
+
+  def set_profile
+    @user = User.find(params[:id])
+  end
+
+  def profile_params
+    params.require(:user).permit(:email, :nickname, :avatar, :avatar_cache)
+  end
 end
