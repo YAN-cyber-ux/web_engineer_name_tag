d72bab981ddad75ae90b269d27aa99dcd2e5af11
yanger <kanayaofficial1129@gmail.com>
2024-06-12 11:44:42 +0900

    Add aws settings

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -6,6 +6,13 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # Choose what kind of storage to use for this uploader:
   storage :file
   # storage :fog
+  if Rails.env.development?
+    storatge :file
+  elsif Rails.env.test?
+    storage :file
+  else
+    storage :fog
+  end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
