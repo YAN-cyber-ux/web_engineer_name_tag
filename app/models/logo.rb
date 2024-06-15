a1849ac5cb21949bb1f359446f334b3f136813a6
yanger <kanayaofficial1129@gmail.com>
2024-06-15 19:53:37 +0900

    Add asociation to logo and name_tag

--- app/models/logo.rb
+++ app/models/logo.rb
@@ -1,2 +1,5 @@
 class Logo < ApplicationRecord
+  has_many :name_tag_logos, dependent: :destroy
+  has_many :name_tags, through: :name_tag_logos
+  
 end
--- app/models/name_tag.rb
+++ app/models/name_tag.rb
@@ -1,5 +1,7 @@
 class NameTag < ApplicationRecord
   belongs_to :user
+  has_many :name_tag_logos, dependent: :destroy
+  has_many :logos, through: :name_tag_logos
 
   validates :publicity, inclusion: { in: [true, false]}
   
