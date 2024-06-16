1d04ea2287392deca401c7d2d7af01135af05b4b
yanger <kanayaofficial1129@gmail.com>
2024-06-16 22:45:10 +0900

    Modify name_tags_controller.rb

--- app/controllers/name_tags_controller.rb
+++ app/controllers/name_tags_controller.rb
@@ -1,31 +1,40 @@
 class NameTagsController < ApplicationController
+  before_action :set_name_tag, only: %i[show edit update destroy]
+
   def index
     @name_tags = NameTag.all.includes(:user).order(created_at: :desc)
   end
 
+  def show; end
+
   def new
     @name_tag = NameTag.new
+    @logos = Logo.all
   end
+
+  def edit; end
+
+  def update; end
 
   def create
     @name_tag = current_user.name_tags.build(name_tags_params)
     if @name_tag.save
-      logo_ids = params[:name_tag][:logo_ids].reject(&:blank?)
-      logo_ids.each do |logo_id|
-        NameTagLogo.create(name_tag: @name_tag, logo_id: logo_id)
       end
       redirect_to name_tags_path, success: t('defaults.message.created', item: NameTag.model_name.human)
     else
       flash.now[:danger] = t('defaults.message.not_created', item: NameTag.model_name.human)
+      @logos = Logo.all
       render :new, status: :unprocessable_entity
     end
   end
 
-  def show
-    @name_tag = NameTag.find(params[:id])
-  end
+  def destroy; end
 
   private
+
+  def set_name_tag
+    @name_tag = NameTag.find(params[:id])
+  end
 
   def name_tags_params
     params.require(:name_tag).permit(:publicity, logo_ids: [])
