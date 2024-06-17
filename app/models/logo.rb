85122f0cf4e99141597c96a3c723818f96763d01
yanger <kanayaofficial1129@gmail.com>
2024-06-17 20:52:23 +0900

    Modify views

--- app/models/logo.rb
+++ app/models/logo.rb
@@ -1,6 +1,6 @@
 class Logo < ApplicationRecord
   mount_uploader :image_url, ImageUrlUploader
 
-  has_many :name_tag_logos
+  has_many :name_tag_logos, dependent: :destroy
   has_many :name_tags, through: :name_tag_logos
 end
